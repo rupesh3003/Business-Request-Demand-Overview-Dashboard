552c163db872a8fb000aa6723cfc322886e8b83c
rupesh3003 <rupeshkumbhakar002@gmail.com>
2025-12-01 04:39:23 +0530

    Add files via upload

---
+++ AdventureWorks_DimDate_Query.sql
@@ -0,0 +1,28 @@
+--Cleased DIM_Date Table
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] as Date,
+  --,[DayNumberOfWeek]
+   
+  [EnglishDayNameOfWeek] as Day,
+  --,[SpanishDayNameOfWeek]
+  --,[FrenchDayNameOfWeek]
+  --,[DayNumberOfMonth]
+  --,[DayNumberOfYear]
+   
+  [WeekNumberOfYear] as WeekNr, 
+  [EnglishMonthName] as Month,
+  Left([EnglishMonthName],3) as MonthShort,
+  --,[SpanishMonthName]
+  --,[FrenchMonthName]
+   
+  [MonthNumberOfYear] as MonthNo, 
+  [CalendarQuarter] as Quarter, 
+  [CalendarYear] as Year
+  --,[CalendarSemester]
+  --,[FiscalQuarter]
+  --,[FiscalYear]
+  --,[FiscalSemester]
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimDate]
+   where CalendarYear >= 2019
